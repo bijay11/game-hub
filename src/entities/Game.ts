318eceb0a57098f41327bb12dd331320772881d3
Bijay Tamang <tmg.bijay11@gmail.com>
2023-07-03 22:02:43 -0400

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,12 +1,16 @@
 import { Platform } from "./Platform";
+import { Genre } from "./Genre";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   slug: string;
   name: string;
+  genres: Genre[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
+  publishers: Publisher[];
 }
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,16 +1,15 @@
-import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useGame } from "../hooks/useGame";
-import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Button, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { ExpandableText } from "../components/ExpandableText";
+import { DefinitionItem } from "../components/DefinitionItem";
+import CriticStore from "../components/CriticScore";
+import { GameAttributes } from "../components/GameAttributes";
 
 export const GameDetailsPage = () => {
-  const [showLess, setShowLess] = useState(true);
-
   const { slug } = useParams();
 
   const { data: game, isLoading, error } = useGame(slug!);
-
   if (isLoading) return <Spinner />;
   if (error || !game) throw error;
 
@@ -18,6 +17,7 @@ export const GameDetailsPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
